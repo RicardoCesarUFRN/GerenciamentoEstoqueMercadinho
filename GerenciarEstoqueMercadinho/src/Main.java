0177ea9e51d15d67765156d01a79a40debc42da7
ricar <ricar@DESKTOP-DNVPBOA>
2023-07-03 17:34:51 -0300

    initial commit

---
+++ GerenciarEstoqueMercadinho/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Menus.produtos = new ArrayList<>();
+		Menus.carregarDados();
+		Menus.menu();
+	}
+
+}
